baf158ee7eb380b99d9a8c7e108c36871f9d1576
GustavoBeckSilva <gustavogbks@gmail.com>
2024-09-05 21:54:49 -0300

    Questão 1, lista 7.

--- L7 - Matrizes/E01_L07.cpp
+++ L7 - Matrizes/E01_L07.cpp
@@ -4,8 +4,40 @@ using namespace std;
 
 int main(){
 
+    setlocale(LC_ALL, "portuguese");
+
+    int fileiras, mesas, option;
+
+    cout << "\n\tInsira a quantidade de fileiras: ";
+    cin >> fileiras;
+
+    if(fileiras <= 0){
+        system("cls");
+        cout << "\n\tERRO: Número de fileiras inválido.";
+        return 0;
+    }
+
+    cout << "\tInsira a quantidade de mesas nas fileiras: ";
+    cin >> mesas;
+
+    if(mesas <= 0){
+        system("cls");
+        cout << "\n\tERRO: Número de mesas inválido.";
+        return 0;
+    }
+
+    int matrizLugares[mesas][fileiras];
 
 
+
+    do{
+        system("cls");
+        cout << "\n\t1 - Ocupar mesa\n\t2 - Desocupar a mesa\n\t3 - Ocupar fileira\n\t4 - Desocupar a fileira\n\t5 - Mostrar mapa das mesas\n\t6 - Sair.\n\n\n\tInsira a opção desejada: ";
+        cin >> option;
+
+
+
+    } while(option != 6);
 
 
     return 0;
